2d3b921d81fe5324238a68b0ae0fd8b6cce6db07
matthewfjerstad <matthew.fjerstad@jamfsoftware.com>
2016-08-05 09:41:11 -0500

    final feature support

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <title>NFL Database</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">NFL Database</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="player.php">Players</a></li>
+            <li><a href="coach.php">Coaches</a></li>
+            <li><a href="team.php">Teams</a></li>
+        </ul>
+    </div>
+</nav>
+
+Welcome to the NFL Database. This website serves as a tool to store information about NFL Players, Coaches and Teams.
